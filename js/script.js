09f33726de579c218959f9b90f722098e330ebaf
IronZac <132574280+IronZac@users.noreply.github.com>
2023-05-19 10:50:25 +0200

    pendiente DOM

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Interacción 1: Personalización con el nombre
+document.addEventListener('DOMContentLoaded', function() {
+  var btnPersonalizar = document.getElementById('btn-personalizar');
+  var mensajePersonalizado = document.getElementById('mensaje-personalizado');
+
+  btnPersonalizar.addEventListener('click', function() {
+    var nombre = prompt('Ingresa tu nombre:');
+    mensajePersonalizado.textContent = 'Hola, ' + nombre + '!';
+  });
+});
+
+// Interacción 2: Mostrar u ocultar instrucciones
+document.addEventListener('DOMContentLoaded', function() {
+  var btnOcultar = document.getElementById('btn-ocultar');
+  var recetaInstrucciones = document.getElementById('receta-instrucciones');
+
+  btnOcultar.addEventListener('click', function() {
+    if (recetaInstrucciones.style.display === 'none') {
+      recetaInstrucciones.style.display = 'block';
+      btnOcultar.textContent = 'Ocult
